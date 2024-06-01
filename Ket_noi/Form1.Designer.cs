cfde7baf9186f142c0ddf866f01e610272e606fb
Sondinh2k3 <ledinhsonx1td@gmail.com>
2024-06-01 12:12:21 +0700

    Chinh sua giao dien, them title

--- Ket_noi/Form1.Designer.cs
+++ Ket_noi/Form1.Designer.cs
@@ -36,15 +36,15 @@
             this.btnThoat = new System.Windows.Forms.Button();
             this.serCOM = new System.IO.Ports.SerialPort(this.components);
             this.lblHienthi = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // chkAmthanh
             // 
             this.chkAmthanh.AutoSize = true;
-            this.chkAmthanh.Location = new System.Drawing.Point(631, 15);
-            this.chkAmthanh.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chkAmthanh.Location = new System.Drawing.Point(561, 12);
             this.chkAmthanh.Name = "chkAmthanh";
-            this.chkAmthanh.Size = new System.Drawing.Size(104, 24);
+            this.chkAmthanh.Size = new System.Drawing.Size(84, 20);
             this.chkAmthanh.TabIndex = 0;
             this.chkAmthanh.Text = "Âm thanh";
             this.chkAmthanh.UseVisualStyleBackColor = true;
@@ -54,9 +54,9 @@
             // 
             this.lblDocGT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.lblDocGT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDocGT.Location = new System.Drawing.Point(97, 124);
+            this.lblDocGT.Location = new System.Drawing.Point(68, 173);
             this.lblDocGT.Name = "lblDocGT";
-            this.lblDocGT.Size = new System.Drawing.Size(277, 81);
+            this.lblDocGT.Size = new System.Drawing.Size(246, 65);
             this.lblDocGT.TabIndex = 1;
             this.lblDocGT.Text = "Đọc giá trị cảm biến";
             this.lblDocGT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,19 +64,17 @@
             // 
             // txtDataReceive
             // 
-            this.txtDataReceive.Location = new System.Drawing.Point(342, 15);
-            this.txtDataReceive.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDataReceive.Location = new System.Drawing.Point(304, 12);
             this.txtDataReceive.Name = "txtDataReceive";
-            this.txtDataReceive.Size = new System.Drawing.Size(210, 26);
+            this.txtDataReceive.Size = new System.Drawing.Size(187, 22);
             this.txtDataReceive.TabIndex = 2;
             this.txtDataReceive.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnKetNoi
             // 
-            this.btnKetNoi.Location = new System.Drawing.Point(148, 286);
-            this.btnKetNoi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnKetNoi.Location = new System.Drawing.Point(112, 301);
             this.btnKetNoi.Name = "btnKetNoi";
-            this.btnKetNoi.Size = new System.Drawing.Size(147, 62);
+            this.btnKetNoi.Size = new System.Drawing.Size(131, 50);
             this.btnKetNoi.TabIndex = 3;
             this.btnKetNoi.Text = "Kết nối";
             this.btnKetNoi.UseVisualStyleBackColor = true;
@@ -84,10 +82,9 @@
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(460, 286);
-            this.btnThoat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnThoat.Location = new System.Drawing.Point(413, 301);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(147, 62);
+            this.btnThoat.Size = new System.Drawing.Size(131, 50);
             this.btnThoat.TabIndex = 4;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -102,26 +99,37 @@
             this.lblHienthi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.lblHienthi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblHienthi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHienthi.Location = new System.Drawing.Point(419, 141);
+            this.lblHienthi.Location = new System.Drawing.Point(371, 184);
             this.lblHienthi.Name = "lblHienthi";
-            this.lblHienthi.Size = new System.Drawing.Size(244, 53);
+            this.lblHienthi.Size = new System.Drawing.Size(217, 42);
             this.lblHienthi.TabIndex = 5;
             this.lblHienthi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblHienthi.Click += new System.EventHandler(this.lblHienthi_Click);
             // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(139, 58);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(405, 41);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Ứng dụng demo project Địa đạo Củ Chi";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(767, 562);
+            this.ClientSize = new System.Drawing.Size(682, 450);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblHienthi);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnKetNoi);
             this.Controls.Add(this.txtDataReceive);
             this.Controls.Add(this.lblDocGT);
             this.Controls.Add(this.chkAmthanh);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -139,6 +147,7 @@
         private System.Windows.Forms.Button btnThoat;
         private System.IO.Ports.SerialPort serCOM;
         private System.Windows.Forms.Label lblHienthi;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
